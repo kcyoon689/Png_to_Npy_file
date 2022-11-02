6d9434ba997d916dd35b9b32b07f3c4052bd79e2
kcyoon689 <kcyoun689@gmail.com>
2022-11-03 02:19:24 +0900

    edit save .npy

--- stack.py
+++ stack.py
@@ -7,10 +7,13 @@ currentDirPath = os.getcwd()
 rawDataDirPath = currentDirPath + '/PngtoNpy/mask/train'
 rawDataFilePath_list = os.listdir(rawDataDirPath)
 rawDataFileFullPath_list = [rawDataDirPath + '/' + file_name for file_name in rawDataFilePath_list]
-saveDataDirPath=os.getcwd() + "/train_mask"
 
 # print(rawDataFileFullPath_list)
 # print(len(rawDataFileFullPath_list)) #train: 19980, test: 560, valid: 560
+
+# Img = np.load(file=saveDataDirPath, allow_pickle=True)
+
+# print(np.shape(Img))
 
 Img=[0] * 19980
 
@@ -19,5 +22,11 @@ for i in range(len(rawDataFileFullPath_list)):
 
 stackImg = np.stack((Img), axis=0)
 
-np.save(saveDataDirPath, "train")
-# print(np.shape(stackImg))
+print(np.shape(stackImg))
+print(stackImg.dtype)
+
+np.save("train_mask.npy", stackImg)
+test = np.load(file=currentDirPath+"/train_mask.npy", allow_pickle=True)
+
+print(np.shape(test))
+print(test.dtype)
